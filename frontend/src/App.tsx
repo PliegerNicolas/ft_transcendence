788de45f756523263012c4559d2638421095d9d8
Mayeul Laneyrie <mlaneyri@student.42.fr>
2024-01-08 18:31:01 +0100

    Playing around with css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,27 @@
-import './App.css'
+import "./App.css";
+import React from "react";
 
-function App() {
+function App()
+{
+	const [count, setCount] = React.useState(0);
+
+	function handleClick()
+	{
+		if (count === 42)
+			return ;
+		setCount(count + 1);
+	}
+
 	return (
 		<div className="App">
-			Transcendence !!!
+			<div className={count != 42 ? "App__Count" : "App__Count--42"}>
+				{ count }
+			</div>
+			<button onClick={handleClick} className="App__Button">
+				Transcendence !!!
+			</button>
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
