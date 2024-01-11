ee0b3eac3368b5a1fa7ec5ecfdf7588752432507
nplieger <plieger05@gmail.com>
2024-01-11 17:48:42 +0100

    Add volume for persistence in database. Added typeorm to nest.js

--- srcs/requirements/backend/app/src/app.module.ts
+++ srcs/requirements/backend/app/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'database',
+      port: 5432,
+      username: 'my_username',
+      password: 'my_password',
+      database: 'my_db',
+      entities: [],
+    synchronize: true,
+    }),
+    UsersModule],
   controllers: [],
   providers: [],
 })
