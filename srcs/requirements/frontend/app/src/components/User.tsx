c91103d7d3d0c86eed8de2c15559997b06fc0451
Mayeul Laneyrie <mlaneyri@student.42.fr>
2024-01-15 19:21:14 +0100

    Started retrieving friendships from API

--- srcs/requirements/frontend/app/src/components/User.tsx
+++ srcs/requirements/frontend/app/src/components/User.tsx
@@ -12,45 +12,72 @@ function User()
 	const api = new Api(`http://${location.hostname}:3450`);
 
 	const [user, setUser] = useState<UserType | null>(null);
-	const [status, setStatus] = useState(0);
+	const [friendShips, setFriendships] = useState([]);
+	const [userStatus, setUserStatus] = useState(0);
+
+	const friendshipHTML = friendShips.map((item: any, index) => {
+		const friend = item.user1.id == id ? item.user2 : item.user1;
+
+		return (
+			<div key={index} className="User__InfoGrid" style={{margin: "0"}}>
+				<div>{friend.id}</div>
+				<div>{friend.username}</div>
+			</div>
+		);
+	});
 
 	async function loadUser() {
 		api.get("/users/" + id)
 			.then(data => setUser(data))
 			.catch(err => {
-				err instanceof Response ? setStatus(err.status) : console.error(err)
+				err instanceof Response ? setUserStatus(err.status) : console.error(err)
 			});
 	}
 	useEffect(() => {loadUser()}, []);
 
+	async function loadFriendships() {
+		api.get("/users/" + id + "/friendships")
+			.then(data => setFriendships(data))
+			.catch(err => {!(err instanceof Response) && console.error(err)});
+	}
+	useEffect(() => {loadFriendships()}, []);
+
 	async function delUser() {
 		api.delete("/users/" + id)
-			.then(() => setStatus(410))
+			.then(() => setUserStatus(410))
 			.catch((err: Error) => {console.error(err)});
 	}
 
 	return (
 		<main className="MainContent">
 			<h2>
-				Profile of #{id}
-				{user != null && ": " + user.profile.firstName + " " + user.profile.lastName}
+				Profile of #{id}:
+				{user != null && " " + user.profile.firstName + " " + user.profile.lastName}
+				<button style={{margin: "-5px 20px"}} onClick={delUser}>Delete</button>
 			</h2>
-			{ status
+			{ userStatus
 				?
 				<p style={{color: "pink"}}>
-					{ status != 410 ? `Cannot load this user: ${status}` : "This user was removed."}
+					{ userStatus != 410 ? `Cannot load this user: ${userStatus}` : "This user was removed."}
 				</p>
 				:
 				user != null &&
 				<div className="p-style">
-					<button onClick={delUser}>Delete</button>
+					<h3>User infos:</h3>
 					<div className="User__InfoGrid">
 						<div>Id</div> <div>{"#" + user.id} </div>
 						<div>Username</div> <div>{user.username} </div>
 						<div>Email</div> <div>{user.email} </div>
-						<div>First Name</div> <div>Name: {user.profile.firstName} </div>
+						<div>First Name</div> <div>{user.profile.firstName} </div>
 						<div>Last Name</div> <div>{user.profile.lastName}</div>
 					</div>
+					<hr />
+					<h3>User friendships:</h3>
+					{
+						friendShips.length ?
+						friendshipHTML :
+						"No friendship was found for this user :'-("
+					}
 				</div>
 			}
 		</main>
