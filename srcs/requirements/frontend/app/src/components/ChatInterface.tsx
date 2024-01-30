e8eb68df128b066b936c39b0d9d3db2ab4d7d01f
Mayeul Laneyrie <mlaneyri@student.42.fr>
2024-01-30 17:37:58 +0100

    Chat interface trololol

--- srcs/requirements/frontend/app/src/components/ChatInterface.tsx
+++ srcs/requirements/frontend/app/src/components/ChatInterface.tsx
@@ -1,12 +1,18 @@
 import { useState, useRef, useEffect } from "react";
 
+import { MsgType } from "../utils/types";
+
 import closeLeft from "../assets/close-left.svg";
 import openLeft from "../assets/open-left.svg";
+import defaultPicture from "../assets/default_profile.png";
+
 import "../styles/chat.css";
 
-function ChatTest()
+// <Chat /> ====================================================================
+
+export default function ChatTest()
 {
-	const dummyChanList = [
+	const chanList = [
 		{
 			id: 1,
 			name: "Un channel",
@@ -39,7 +45,7 @@ function ChatTest()
 		}
 	];
 
-	const dummyMsgList = [
+	const [msgList, setMsgList] = useState([
 		{
 			uid: 1,
 			username: "mama",
@@ -69,6 +75,18 @@ function ChatTest()
 			date: "18:04"
 		},
 		{
+			uid: 3,
+			username: "Mimi",
+			content: `ertryurtyutyutyrrte`,
+			date: "18:04"
+		},
+		{
+			uid: 3,
+			username: "Mimi",
+			content: `wncbwxcnbvxc,nwb<v`,
+			date: "18:04"
+		},
+		{
 			uid: 1,
 			username: "mama",
 			content: "Tu racontes de la merde @Mimi",
@@ -81,37 +99,68 @@ function ChatTest()
 			date: "18:06"
 		},
 		{
+			uid: 2,
+			username: "Momo",
+			content: "Oui, c'est pas bieeeen",
+			date: "18:07"
+		},
+		{
 			uid: 3,
 			username: "Mimi",
 			content: "Ah bon d'accord",
 			date: "18:07"
 		},
-	]
+		{
+			uid: 3,
+			username: "Mimi",
+			content: `Ce message ne se connecte pas avec le précédent parce que la
+			date est différente`,
+			date: "18:08"
+		},
+	]);
 
 	const [showChanList, setShowChanList] = useState(1);
-
 	const [currentChanId, setCurrentChanId] = useState(1);
+	const [inputValue, setInputValue] = useState("");
 
-	const currentChan = dummyChanList
+	const currentChan = chanList
 		.filter(item => item.id === currentChanId)[0];
 
 	/*
 	** These lines are desirable to auto-scroll at bottom of chat.
 	*/
-	const ref = useRef<HTMLDivElement>(null);
+	const convoRef = useRef<HTMLDivElement>(null);
+	const anchorRef = useRef<HTMLDivElement>(null);
+	useEffect(() => anchorRef.current?.scrollIntoView(), [msgList]);
 
-	useEffect(() => ref.current?.scrollIntoView({behavior: "smooth"}), []);
+	function handleInputChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+		const elem: HTMLDivElement = convoRef.current!;
+
+		if (elem?.scrollHeight - elem?.scrollTop === elem?.clientHeight)
+			console.log("AT BOTTOM");
+
+		if (e.currentTarget.value.slice(-1) !== "\n") {
+			setInputValue(e.currentTarget.value);
+			return ;
+		}
+		if (inputValue === "")
+			return ;
+		setMsgList(prev =>
+			[...prev, {uid: 1, username: "mama", content: inputValue, date: "00:00"}]
+		);
+		setInputValue("");
+	}
 
 	return (
 		<main className="MainContent Chat">
 		{
 			showChanList ?
 			<div className={
-				"Chat__Channels"
+				"Chat__Sidebar"
 				+ (showChanList === -1 ? " collapse" : "")
 				+ (showChanList === 2 ? " expand" : "")
 			}>
-				<h3 className="Chat__ChannelsTitle">
+				<h3 className="Chat__SidebarTitle">
 					Your channels
 					<div
 						className="Chat__Collapse"
@@ -122,18 +171,18 @@ function ChatTest()
 						<img src={closeLeft} />
 					</div>
 				</h3>
-				<div className="Chat__ChannelList">
+				<div className="Chat__ChanList">
 				{
-					dummyChanList.map(item =>
+					chanList.map(item =>
 						<div
 							key={item.id}
-							className={`Chat__ChannelListItem${item.id === currentChanId ? "--curr" : ""}`}
+							className={`Chat__ChanListItem${item.id === currentChanId ? "--curr" : ""}`}
 							onClick={() => {setCurrentChanId(item.id)}}
 						>
-							<div className="Chat__ChannelListItemName">{item.name}</div>
+							<div className="Chat__ChanListItemName">{item.name}</div>
 							{
 								item.size > 2 &&
-								<div className="Chat__ChannelListItemSize">
+								<div className="Chat__ChanListItemSize">
 									{item.size + " members"}
 								</div>
 							}
@@ -158,25 +207,32 @@ function ChatTest()
 						{currentChan.name}
 					</div>
 				</div>
-				<div className="Chat__Convo">
-				{
-					dummyMsgList.map((item, index) => 
-						<div key={index} className={`Msg${item.uid === 1 ? "--me" : ""}`}>
-							<div
-								className="Msg__Sender"
-								style={{color: `hsl(${(360 / currentChan.size) * item.uid} 80% 80%)`}}
-							>
-								{item.username}
-							</div>
-							{item.content}
-						</div>
-					)
-				}
-				<div ref={ref} />
+				<div className="Chat__Convo" ref={convoRef}>
+					<div className="notice-msg Chat__Start">
+						{
+							currentChan.size === 2 ?
+							`Start of your conversation with ${currentChan.name}` :
+							`Start of channel « ${currentChan.name} »`
+						}
+						<hr />
+					</div>
+					{
+					msgList.map((item, index) =>
+						<Msg
+							key={index}
+							data={item}
+							prev={index ? msgList[index - 1] : null}
+							next={index < msgList.length ? msgList[index + 1] : null}
+							size={currentChan.size}
+						/>)
+					}
+					<div ref={anchorRef} />
 				</div>
 				<div className="Chat__Input">
 					<textarea
 						placeholder={`Send a message to « ${currentChan.name} »`}
+						value={inputValue}
+						onChange={handleInputChange}
 					/>
 				</div>
 			</div>
@@ -184,4 +240,42 @@ function ChatTest()
 	);
 }
 
-export default ChatTest;
+// <Msg /> =====================================================================
+
+function Msg(
+	{data, prev, next, size}:
+	{data: MsgType, prev: MsgType | null, next: MsgType | null, size: number}
+)
+{
+	const connectPrev = prev && prev.uid === data.uid && prev.date === data.date;
+	const connectNext = next && next.uid === data.uid && next.date === data.date;
+
+	return (
+		<div className={
+			"Msg"
+			+ (data.uid === 1 ? " me" : "")
+			+ (connectPrev ? " connectPrev": "")
+			+ (connectNext ? " connectNext" : "")
+		}>
+			<div className="Msg__PictureDiv">
+				<img src={defaultPicture} />
+			</div>
+			<div>
+			{
+				<div className="Msg__Info">
+					<span
+						className="Msg__Sender"
+						style={{color: `hsl(${(360 / size) * data.uid} 80% 80%)`}}
+					>
+						{data.username}
+					</span>
+					<span className="notice-msg Msg__Date">
+						{data.date}
+					</span>
+				</div>
+			}
+				{data.content}
+			</div>
+		</div>
+	);
+}
