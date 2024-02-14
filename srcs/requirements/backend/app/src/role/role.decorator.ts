7116028012c181f11ead313e0d9d4abb6992512e
Julien BOYER <julboyer@paul-f4Br2s2.clusters.42paris.fr>
2024-02-14 13:54:12 +0100

    added passport in channels controller

--- srcs/requirements/backend/app/src/role/role.decorator.ts
+++ srcs/requirements/backend/app/src/role/role.decorator.ts
@@ -1,3 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 
-export const Role = (...args: string[]) => SetMetadata('role', args);
+export const Role = Reflector.createDecorator<string[]>();
