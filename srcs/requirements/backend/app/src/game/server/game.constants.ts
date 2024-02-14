abc90812ada85474a58b326767273b42644cbbb7
anbourge <anbourge@student.42.fr>
2024-02-14 16:20:52 +0100

    game is now responsive, prettier and more fluid

--- srcs/requirements/backend/app/src/game/server/game.constants.ts
+++ srcs/requirements/backend/app/src/game/server/game.constants.ts
@@ -1,14 +1,14 @@
-export const WINDOW_WIDTH = 900;
-export const WINDOW_HEIGHT = 600;
+export const WINDOW_WIDTH = 1280;
+export const WINDOW_HEIGHT = 720;
 
-export const PADDLE_HEIGHT = 100;
+export const PADDLE_HEIGHT = 120;
 export const PADDLE_WIDTH = 20;
 export const PADDLE_SPEED = 12;
 
-export const BALL_SIZE = 10;
-export const BALL_SPEED = 0.8;
+export const BALL_SIZE = 15;
+export const BALL_SPEED = 0.6;
 
 export const MAX_SCORE = 5;
 
-export const FRAME_RATE = 60;
+export const FRAME_RATE = 120;
 
--- srcs/requirements/frontend/app/src/components/Game/OnlinePlay.tsx
+++ srcs/requirements/frontend/app/src/components/Game/OnlinePlay.tsx
@@ -4,13 +4,13 @@ import uuid from 'react-uuid'
 
 import "../../styles/play.css";
 
-const WINDOW_WIDTH = 900;
-const WINDOW_HEIGHT = 600;
+const WINDOW_WIDTH = 1280;
+const WINDOW_HEIGHT = 720;
 
-const PADDLE_HEIGHT = 100;
+const PADDLE_HEIGHT = 120;
 const PADDLE_WIDTH = 20;
 
-const BALL_SIZE = 10;
+const BALL_SIZE = 15;
 
 const MAX_SCORE = 5;
 
@@ -73,63 +73,82 @@ const OnlineGame = () => {
 		const gameContext = gameCanvas?.getContext('2d');
 		
 		const drawGame = (new_gameState: InputPayloads) => {
+			gameContext!.textAlign = "center";
 
-			const drawBoardDetails = () => {
+			const drawBackground = () => {
+				gameContext!.fillStyle = "#000";
+				gameContext!.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+			}
+
+			const drawLines = () => {
 				for (var i = 0; i < WINDOW_HEIGHT; i += 30) {
 					gameContext!.fillStyle = "#fff";
-					gameContext!.fillRect((WINDOW_WIDTH / 2) - 3, i + 10, 6, 15);
+					gameContext!.fillRect((WINDOW_WIDTH / 2) - 6, i + 10, 6, 15);
 				}
-				gameContext!.font = "60px Orbitron";
-				gameContext!.fillText(new_gameState.score.player1.toString(), (WINDOW_WIDTH / 2) - 50, 60);
-				gameContext!.fillText(new_gameState.score.player2.toString(), (WINDOW_WIDTH / 2) + 30, 60);
+			}
+
+			const drawPlayersName = () => {
 				gameContext!.font = "30px Orbitron";
 				if (playerNumber === 1) {
-					gameContext!.fillText("You", 150, 40);
-					gameContext!.fillText(new_gameState.player2ID, (WINDOW_WIDTH / 2) + 150, 40);
+					gameContext!.fillText("You", (WINDOW_WIDTH / 4), 40);
+					gameContext!.fillText(new_gameState.player2ID, (WINDOW_WIDTH / 1.333), 40);
 				}
 				else if (playerNumber === 2) {
-					gameContext!.fillText(new_gameState.player1ID, 150, 40);
-					gameContext!.fillText("You", (WINDOW_WIDTH / 2) + 150, 40);
+					gameContext!.fillText(new_gameState.player1ID, (WINDOW_WIDTH / 4), 40);
+					gameContext!.fillText("You", (WINDOW_WIDTH / 1.333), 40);
 				}
+			}
+
+			const drawScores = () => {
+				gameContext!.font = "60px Orbitron";
+				gameContext!.fillText(new_gameState.score.player1.toString(), (WINDOW_WIDTH / 2) - 50, 60);
+				gameContext!.fillText(new_gameState.score.player2.toString(), (WINDOW_WIDTH / 2) + 50, 60);
+			}
+
+			const drawBoardDetails = () => {
+				drawBackground();
+				drawLines();
+				drawPlayersName();
+				drawScores();
 			}
 	
 			const drawGameOver = () => {
+				drawBackground();
 				gameContext!.font = "80px Orbitron";
 				gameContext!.fillStyle = "#fff";
 				if ((new_gameState.score.player1 >= MAX_SCORE && playerNumber === 1) || (new_gameState.score.player2 >= MAX_SCORE && playerNumber === 2)) {
-					gameContext!.fillText("You won", (WINDOW_WIDTH / 2) - 200, (WINDOW_HEIGHT / 2));
+					gameContext!.fillText("You won", (WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2));
 				}
 				else {
-					gameContext!.fillText("You lost", (WINDOW_WIDTH / 2) - 200, (WINDOW_HEIGHT / 2));
+					gameContext!.fillText("You lost", (WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2));
 				}
 			}
 	
 			const drawPause = () => {
 				gameContext!.font = "60px Orbitron";
 				gameContext!.fillStyle = "#fff";
-				gameContext!.fillText("Game Paused", (WINDOW_WIDTH / 2) - 150, (WINDOW_HEIGHT / 2));
+				gameContext!.fillText("Game Paused", (WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2));
+			}
+
+			const drawGameState = () => {
+				gameContext!.fillStyle = "#fff";
+				gameContext!.fillRect(new_gameState.player1.x, new_gameState.player1.y, PADDLE_WIDTH, PADDLE_HEIGHT);
+				gameContext!.fillRect(new_gameState.player2.x, new_gameState.player2.y, PADDLE_WIDTH, PADDLE_HEIGHT);
+				gameContext!.fillRect(new_gameState.ball.x, new_gameState.ball.y, BALL_SIZE, BALL_SIZE);	
 			}
 	
-			gameContext!.fillStyle = "#000";
-			gameContext!.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-				  
 			drawBoardDetails();
+			drawGameState();
 			if (gameOver) {
 				drawGameOver();
 			}
 			else if (!gameState) {
 				drawPause();
 			}
-			gameContext!.fillStyle = "#fff";
-			gameContext!.fillRect(new_gameState.player1.x, new_gameState.player1.y, PADDLE_WIDTH, PADDLE_HEIGHT);
-			gameContext!.fillRect(new_gameState.player2.x, new_gameState.player2.y, PADDLE_WIDTH, PADDLE_HEIGHT);
-			gameContext!.fillRect(new_gameState.ball.x, new_gameState.ball.y, BALL_SIZE, BALL_SIZE);	
-	
-			//gameContext!.fillStyle = 'black';
-			//gameContext!.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 		}
 
 		const drawTimer = () => {
+			gameContext!.textAlign = "center";
 			gameContext!.fillStyle = "#000";
 			gameContext!.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 			gameContext!.fillStyle = "#fff";
@@ -187,9 +206,10 @@ const OnlineGame = () => {
 				console.log('Start game');
 				drawTimer();
 				setGameState(true);
+				setGameOver(false);
 			});
 			socket.on('updateGame', (new_gameState: InputPayloads) => {
-				if (gameContext)
+				if (gameContext && !gameOver)
 					requestAnimationFrame(() => drawGame(new_gameState));
 			});
 			socket.on('gameOver', (new_gameState: InputPayloads) => {
@@ -246,8 +266,8 @@ const OnlineGame = () => {
 	return (
 		<div>
 			{lobby.length === 0 ? <div>
-				<button onClick={lobbyCreateHandler}>Create Lobby</button>
-				<div>
+				<button className="Create-lobby-button" onClick={lobbyCreateHandler}>Create Lobby</button>
+				<div className="Lobby-list">
 					<span>Lobby name : </span>
 					<input
 						type="text"
@@ -263,8 +283,8 @@ const OnlineGame = () => {
 					width={WINDOW_WIDTH}
 					height={WINDOW_HEIGHT}
 					className="Canvas"></canvas>
-					<button onClick={initGameHandler}>Start game</button>
-					<p>Lobby name : {lobby}</p>
+					<button className="Start-button" onClick={initGameHandler}>Start game</button>
+					<p>{lobby}</p>
 			</div> }
 
 		</div>
