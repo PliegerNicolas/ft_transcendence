9e48f60afdec848410a7a668f88e1f4b2569f6df
nplieger <plieger05@gmail.com>
2024-03-18 22:35:31 +0100

    Some small corrections

--- srcs/requirements/frontend/app/vite.config.ts
+++ srcs/requirements/frontend/app/vite.config.ts
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs';
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
     	port: 3030,
 		https: {
-			key: fs.readFileSync('./ssl_certs/localhost.key'),
-			cert: fs.readFileSync('./ssl_certs/localhost.crt'),
+			key: fs.readFileSync(`./ssl_certificates/dev.key`),
+			cert: fs.readFileSync(`./ssl_certificates/dev.crt`),
 		},
 	},
 	preview: {
