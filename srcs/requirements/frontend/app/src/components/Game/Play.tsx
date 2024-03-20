92ec5232393e920e666952545a2048aece233ff4
Antoine BOURGEOIS <anbourge@paul-f4Br6s1.clusters.42paris.fr>
2024-03-20 18:20:20 +0100

    ladder username clickable

--- srcs/requirements/frontend/app/src/components/Game/Play.tsx
+++ srcs/requirements/frontend/app/src/components/Game/Play.tsx
@@ -5,7 +5,7 @@ import OnlineGame  from './OnlinePlay'
 
 import "../../styles/play.css";
 import { MyContext } from '../../utils/contexts.ts';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { useGet } from '../../utils/hooks.ts';
 import { UserType } from '../../utils/types.ts';
 
@@ -225,7 +225,11 @@ function Ladder() {
 				getUsers.data.sort((a: UserType, b: UserType) => b.profile.elo - a.profile.elo).map((user: UserType, index: number) =>
 					<div className="Ladder__Item">
 						<div className="Ladder__Index">#{index + 1} {index + 1 === 1 ? <>🏆</> : <></>}</div>
-						<div className="Ladder__Username">{user.username} </div>
+						<div className="Ladder__Username">
+							<Link to={"/user/" + user.username}>
+								<span>{user.username}</span>
+							</Link>
+						</div>
 						<div className="Ladder__Elo">{user.profile.elo}</div>
 					</div>	
 				)
