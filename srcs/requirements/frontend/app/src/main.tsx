3886aad3f42974a795bc6a42e8902755382c5aa6
Mayeul Laneyrie <mlaneyri@student.42.fr>
2024-03-21 17:43:55 +0100

    NIQUE SA MERE

--- srcs/requirements/frontend/app/src/main.tsx
+++ srcs/requirements/frontend/app/src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -9,9 +8,7 @@ import "./index.css";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 );
