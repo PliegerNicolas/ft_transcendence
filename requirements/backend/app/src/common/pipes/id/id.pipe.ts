6734e094d3c0381662cfb34262919303ef1e692a
Nicolas PLIEGER <plieger05@gmail.com>
2024-03-22 14:11:32 +0100

    Corrected Muted Interceptor 500 error

--- requirements/backend/app/src/common/pipes/id/id.pipe.ts
+++ requirements/backend/app/src/common/pipes/id/id.pipe.ts
@@ -4,7 +4,7 @@ import { BigintPipe } from '../bigint/bigint.pipe';
 @Injectable()
 export class IdPipe extends BigintPipe {
 
-	transform(value: string, metadata: ArgumentMetadata): bigint {
+	transform(value: string, metadata?: ArgumentMetadata): bigint {
 		const transformedValue: bigint = super.transform(value, metadata);
 
 		if (transformedValue <= 0) throw new BadRequestException(`Validation failed (ID should be strictly positive)`);
