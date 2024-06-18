5ca6d1b448259151a59d70d69b622a7840df2a9e
PliegerNicolas <plieger05@gmail.com>
2024-06-18 19:46:33 +0200

    small changes

--- requirements/backend/app/src/database-config.ts
+++ requirements/backend/app/src/database-config.ts
@@ -4,9 +4,9 @@ export const dbConfig: TypeOrmModuleOptions = {
 	type: 'postgres',
 	host: 'database',
 	port: parseInt(process.env.DB_PORT ?? '5432'),
-	username: process.env.DB_USER ?? 'default_user',
-	password: process.env.DB_PASSWORD ?? 'default_password',
-	database: process.env.DB_NAME ?? 'default_db',
+	username: process.env.POSTGRES_USER ?? 'default_user',
+	password: process.env.POSTGRES_PASSWORD ?? 'default_password',
+	database: process.env.POSTGRES_DB ?? 'default_db',
 	entities: [__dirname + '/**/*.entity{.ts,.js}'],
 	synchronize: true,
 	//logging: true, // TEMP
